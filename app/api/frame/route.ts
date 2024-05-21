a1529fe5081c83a31e4c44f9975e55986776f39c
cleanerzkp <104455005+cleanerzkp@users.noreply.github.com>
2024-05-21 13:49:43 +0200

    update

--- app/api/frame/route.ts
+++ app/api/frame/route.ts
@@ -1,4 +1,4 @@
-import { FrameRequest, getFrameMessage, getFrameHtmlResponse, FrameButtonMetadata, FrameMetadataType } from '@coinbase/onchainkit/frame';
+import { FrameRequest, getFrameHtmlResponse, FrameButtonMetadata, FrameMetadataType } from '@coinbase/onchainkit/frame';
 import { NextRequest, NextResponse } from 'next/server';
 import { NEXT_PUBLIC_URL } from '../../config';
 
@@ -8,13 +8,8 @@ const darkModeImageUrl = "https://ipfs.io/ipfs/bafybeiakdyxvbtavc5jd4ygbo32sofgl
 
 async function getResponse(req: NextRequest): Promise<NextResponse> {
   const body: FrameRequest = await req.json();
-  const { isValid, message } = await getFrameMessage(body, { neynarApiKey: 'NEYNAR_ONCHAIN_KIT' });
 
-  if (!isValid) {
-    return new NextResponse('Message not valid', { status: 500 });
-  }
-
-  const state = message.state ? JSON.parse(decodeURIComponent(message.state.serialized)) : { mode: 'gm' };
+  const state = body.untrustedData?.state ? JSON.parse(decodeURIComponent(body.untrustedData.state)) : { mode: 'gm' };
   const mode = state.mode || 'gm';
 
   let imageUrl = gmImageUrl;
